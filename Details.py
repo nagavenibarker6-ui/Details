8db96bf4cf95148418b85cc43e791f5ae6ac39da
Nagaveni.Barker <nagavenibarker6@gmail.com>
2025-10-30 14:59:39 +0530

    Create Details.py

---
+++ Details.py
@@ -0,0 +1,8 @@
+a=int(input("Enter  the student name:")//NagaveniBarker
+b=int(input("Enter  the student age:")
+c=int(input("Enter  the student USN:")
+d=int(input("Enter  the student address:")
+      print(f"Name:{a}")
+      print(f"Age:{b}")
+      print(f"USN:{c}")
+      print(f"Address:{d}")
